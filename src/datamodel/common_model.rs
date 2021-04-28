91357c3b7aaace751240c14932f36c45686bd2f5
N1ghtStorm <diman1ghtly@gmail.com>
2021-04-28 23:42:44 +0300

    add data

--- src/datamodel/common_model.rs
+++ src/datamodel/common_model.rs
@@ -1,7 +1,9 @@
 use std::sync::Arc;
+use std::sync::Mutex;
 
 mod core_model;
 
 pub struct GraphCollectionFacade {
-    pub graph_collection: Arc<Vec<Box<dyn core_model::Graph>>>
+    //pub graph_collection: Arc<Mutex<Vec<Box<dyn core_model::Graph>>>>
+    pub in_memory_graph_collection: Arc<Mutex<Vec<core_model::InMemoryGraph>>>
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::sync::Arc;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use std::sync::Mutex;
 
 #[path = "./datamodel/core_model.rs"]
 mod core_model;
@@ -9,19 +10,32 @@ mod common_model;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let mut graph_collection = initialize_graph_collection();
+    let data = web::Data::new(initialize_graph_collection());
 
-    HttpServer::new(|| {
+    HttpServer::new( move || {
         App::new()
-            //.register_controllers()
-            //.service(cat_controller::get_cats)
+            .app_data(data.clone())
+            .service(hello)
+            .service(hi)
     })
     .bind("127.0.0.1:8080")?
     .run()
     .await
 }
 
+#[get("/create_graph")]
+async fn hello(arc_graph_col_fac: web::Data<common_model::GraphCollectionFacade>, req_body: String) -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[get("/")]
+async fn hi() -> impl Responder {
+    HttpResponse::Ok().body("Db Server works! :)")
+}
+
 /// initialize common graph collection for all programm lifetime
-fn initialize_graph_collection() -> Arc<common_model::GraphCollectionFacade> {
-    Arc::new(common_model::GraphCollectionFacade {graph_collection: Arc::new(Vec::new())})
+fn initialize_graph_collection() -> Arc<Mutex<common_model::GraphCollectionFacade>> {
+    Arc::new(Mutex::new(common_model::GraphCollectionFacade {
+        in_memory_graph_collection: Arc::new(Mutex::new(Vec::new()))
+    }))
 }
